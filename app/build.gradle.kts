387454367181b60c5286cdf26f3802318714a9f5
Gabri <gperezpereira@danielcastelao.org>
2023-10-04 10:30:54 +0200

    Practicando MutableState

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.example.laprimera"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.laprimera"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -48,9 +48,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/example/laprimera/MainActivity.kt
+++ app/src/main/java/com/example/laprimera/MainActivity.kt
@@ -5,17 +5,33 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.laprimera.ui.theme.LaPrimeraTheme
+import java.util.function.Function
+import kotlin.jvm.internal.FunctionReference
 
 var name:String = "Android" //var es para variables
 
@@ -30,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LaPrimeraTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = Color.Magenta) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color(100,0,255)) {
                     Saludo(name)
                 }
             }
@@ -39,6 +55,33 @@ class MainActivity : ComponentActivity() {
 
         ///Empieza la logica de la aplicación
         Log.d(TAG,"Estoy en Create")
+
+        calcular(a=3,b=5,
+            fun(a:Int,b:Int){
+                val suma=a+b
+                Log.d("Calcular",suma.toString())
+              })
+
+        calcular(4,1,
+            fun(x,y){
+                val resta=x-y
+                Log.d("Calcular",resta.toString())
+             })
+
+        calcular2{
+               Log.d("Calcular","Yo no hago nada")
+            }
+        
+
+    }
+
+    fun calcular(a:Int=0,b:Int=0, operacion: (x:Int,y:Int)->Unit){
+        //val operacion=a+b
+        operacion(a,b)
+    }
+
+    fun calcular2(a:Int=0,b:Int=0, operacion: ()->Unit){
+        operacion()
     }
 
     override fun onStart(){
@@ -70,24 +113,61 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         Log.e(TAG,"He llegado al Destroy")
     }
+
 }
 
 @Composable
 fun Saludo(name: String, modifier: Modifier = Modifier) {
-    var miColor=Color.Cyan
+    var numbers = remember {mutableStateOf(0)}  // esto hace que siempre que cambie el valor de numbers se va a actualizar, remember es un observer
+    //al ponerle el cero el compilador ya sabe que es un entero
+
+    var miColor= Color(255,222,0)
+    var saludo=stringResource(R.string.greeting)
     Column {
+        Image(
+            painter = painterResource(R.drawable.pikachu),
+            contentDescription= "imagen"
+        )
 
         Text(
-            text = "Hola, $name!",
+            text = "$saludo, $name",
             modifier = modifier,
             fontSize = 40.sp,
             color = miColor
         )
 
         Text(
-            text = "Hola, $name!",
+            text = "Soy Pikachu!",
             modifier = modifier,
             fontSize = 60.sp,
+            color = Color.Red
+        )
+        
+        Button(
+            onClick = {
+                numbers.value=(0..10).random()
+                Log.d("calcular","Click!!!!!")
+            },
+            modifier= Modifier
+                .height(90.dp)
+                .width(150.dp),
+            colors=ButtonDefaults.buttonColors(miColor)
+        ) {
+            Text(text = "Pincha aquí",
+                fontSize = 15.sp,
+                color = Color.Red
+                )
+            Image(
+                painter = painterResource(R.drawable.baseline_cruelty_free_24),
+                contentDescription= "imagen",
+                modifier=modifier.height(48.dp).width(48.dp)
+                )
+            }
+
+        Text(
+            text = "Numero random: ${numbers.value}",
+            modifier = modifier,
+            fontSize = 30.sp,
             color = Color.Yellow
         )
     }
@@ -97,6 +177,27 @@ fun Saludo(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LaPrimeraTheme {
-        Saludo(name)
+        Saludo("prueba")
     }
+}
+
+@Composable
+fun chat(){
+    TODO("Not yet implemented")
+}
+@Composable
+fun login(){
+    //texto y boton para loguear
+    texto_descriptivo(texto = "Fallo de login")
+}
+@Composable
+fun texto_descriptivo(texto:String){
+    //texto descriptivo
+    Text(text = texto)
+}
+@Composable
+fun interfazUsuario(){
+    login()
+    texto_descriptivo(texto = "Hola texto")
+    chat()
 }
